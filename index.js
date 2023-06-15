fc35cfb69a3230693d316a796ac17d60c39f4e85
wzg_work <2978968560@qq.com>
2023-06-15 11:30:32 +0800

    完善

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(' ');
 
 /** 复制的数据历史列表 */
 const dataList = [];
-const timeSpace = 30;
+const timeSpace = 130;
 function takeData(){
     let res = '';
     try {
@@ -19,14 +19,18 @@ function takeData(){
         res = '';
     }
     if(!res) return;
-    if(dataList[0] && (res === dataList[0].content)) return;
+    // if(dataList[0] && (res === dataList[0].content)) return;
+    if(dataList.find(item=>{
+        // 与其中有的则不记录
+        return item.content === res;
+    })) return;
     const data = {
         initData:new Date().getTime(),
         content:res,
     };
     data.dateString = dayjs(data.initData).format("YYYY-MM-DD HH:mm:ss");
     dataList.unshift(data);
-    if(dataList.length > 3) {
+    if(dataList.length > 5) {
         dataList.pop();
     }
     console.log(colors.rainbow(`${data.dateString}`),':',`${data.content}`);
