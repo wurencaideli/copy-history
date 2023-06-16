92610c74e912ea5e8c181836261b99cd76451083
wzg_work <2978968560@qq.com>
2023-06-16 14:39:48 +0800

    完善将数据存到本地

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 const clipboardy = require('clipboardy');
 const dayjs = require('dayjs');
 const colors = require('colors');
+const makeDir = require('make-dir');
+const fs = require('fs');
+const path = require('path');
 
 console.log(colors.cyan('以下是您粘贴板的历史列表'));
 console.log(' ');
@@ -11,6 +14,7 @@ console.log(' ');
 /** 复制的数据历史列表 */
 const dataList = [];
 const timeSpace = 130;
+const maxLength = 300;
 function takeData(){
     let res = '';
     try {
@@ -20,7 +24,7 @@ function takeData(){
     }
     if(!res) return;
     // if(dataList[0] && (res === dataList[0].content)) return;
-    if(dataList.find(item=>{
+    if(dataList.slice(0,5).find(item=>{
         // 与其中有的则不记录
         return item.content === res;
     })) return;
@@ -30,11 +34,36 @@ function takeData(){
     };
     data.dateString = dayjs(data.initData).format("YYYY-MM-DD HH:mm:ss");
     dataList.unshift(data);
-    if(dataList.length > 5) {
+    if(dataList.length > maxLength) {
         dataList.pop();
     }
-    console.log(colors.rainbow(`${data.dateString}`),':',`${data.content}`);
+    console.log(colors.rainbow(`${data.dateString}`));
+    console.log(`${data.content}`);
+    saveData();
+}
+/** 
+ * 将数据存起来
+ * 按照程序启动时的分钟记录
+ *  */
+const dayIns = dayjs(new Date());
+async function saveData(){
+    let path_ = path.join(
+        __dirname,
+        'data-files',
+        String(dayIns.year()),
+        String(dayIns.month()),
+        String(dayIns.date()),
+    );
+    await makeDir(path_);
+    fs.writeFileSync(
+        path.join(path_,`${dayIns.hour()}点${dayIns.minute()}分.txt`),
+        dataList.map(item=>{
+            return `${item.dateString}\n${item.content}`;
+        }).join('\n\n'), 
+        "utf8",
+    );
 }
 setInterval(()=>{
     takeData();
 },timeSpace);
+takeData();
